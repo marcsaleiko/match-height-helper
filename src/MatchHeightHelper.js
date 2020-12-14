acc3dfa191efeeaa4e1ef2069ccb34c6edf01e94
Marc Saleiko <marc.saleiko@komadesign.de>
2020-12-14 12:04:13 +0100

    [FEATURE] Adds matchheight options

--- src/MatchHeightHelper.js
+++ src/MatchHeightHelper.js
@@ -4,23 +4,39 @@ window.MatchHeightHelper = function () {
     defaultTriggerSelector: '.match-height',
     defaultTargetSelector: '.match-height-target'
   };
+  var matchHeightOptions = {
+    byRow: true,
+    property: 'height',
+    target: null,
+    remove: false
+  };
+
+  app.setMatchHeightOptions = function(options) {
+    matchHeightOptions = $.extend( matchHeightOptions, options );
+  }
 
   app.init = function (options) {
-    $.extend(settings, options); 
+    settings = $.extend(settings, options);
     
     // this one walks through all match-height class
     // instances and applies the match-height plugin to every match-height-target
     // this is a better approach than .match-height .match-height-target selector
     // because the height will be calculated for each match-height context individually
     $(settings.defaultTriggerSelector).each(function () {
-      $(this).find(settings.defaultTargetSelector).matchHeight();
+      $(this).find(settings.defaultTargetSelector).matchHeight(matchHeightOptions);
     });
   };
 
-  app.registerSet = function (triggerSelector, targetSelector) {
+  app.registerSet = function (triggerSelector, targetSelector, matchHeightOptionsOverrides ) {
     if (typeof triggerSelector !== 'undefined' && typeof targetSelector !== 'undefined') {
+
+      var customMatchHeightOptions = matchHeightOptions;
+      if( typeof matchHeightOptionsOverrides !== 'undefined' ) {
+        customMatchHeightOptions = $.extend( matchHeightOptions, matchHeightOptionsOverrides );
+      }
+
       $(triggerSelector).each(function () {
-        $(this).find(targetSelector).matchHeight();
+        $(this).find(targetSelector).matchHeight(customMatchHeightOptions);
       });
     }
   };
